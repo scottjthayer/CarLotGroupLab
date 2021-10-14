73777ca1d944eacde7be7ba6c941b3331d2d1cb3
scott <scottjthayer@gmail.com>
2021-10-14 16:18:11 -0400

    second commit for scott branch

--- CarLot.cs
+++ CarLot.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using Validator;
+using System.Linq;
+
 namespace Car_Lot_Group_Lab
 {
     class CarLot
     {
-        public List<Car> CarList()
+        public static void CarList()
         {
             List<Car> list = new List<Car>
             {
@@ -17,23 +19,63 @@ namespace Car_Lot_Group_Lab
                 new UsedCar("Toyota", "Prius", 2016, 28000, 49000),
                 new UsedCar("Dodge", "Ram", 2019, 26000, 40000),
             };
-            foreach (Car c in list)
+            bool run = true;
+            while (run == true)
             {
-                Console.WriteLine(c.ToString());
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {list[i]}");
+                }
+
+                Console.WriteLine($"{list.Count + 1}. Add a car");
+                Console.WriteLine($"{list.Count + 2}. Quit");
+
+                PurchaseCar(list);
+                //foreach (Car c in list)
+                //{
+                //    Console.WriteLine(c.ToString());
+                //}
+                Console.WriteLine("Have a great day!");
+                break;
             }
 
+        }
+
+        public static List<Car> PurchaseCar(List<Car> list)
+        {
+            while (true)
+            {
+                Console.WriteLine("Which option would you like?");
+                int choice = (int)Validator.Validator.GetNumber();
+                if (choice == list.Count + 1)
+                {
+                    AddCar(list);
+                }
+                else if (choice == list.Count + 2)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"{list[choice - 1]}");
+                    bool buy = Validator.Validator.GetContinue("Would you like to buy this car?");
+                    if (buy == true)
+                    {
+                        list.Remove(list[choice - 1]);
+                        Console.WriteLine("Excellent. Our finance dept. will be in touch with you shortly.");
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
             return list;
         }
-
-        public List<Car> PurchaseCar(List<Car> list, Car car)
+        public static List<Car> AddCar(List<Car> c)
         {
-
-        }
-
-
-        public List<Car> AddCar(List<Car> c)
-        {
-            bool add = Validator.Validator.GetContinue("Would you like to add a car? Y/N?");
+            bool add = Validator.Validator.GetContinue("Would you like to add a car?");
             Car result = new Car();
             while (add == true)
             {
@@ -57,9 +99,16 @@ namespace Car_Lot_Group_Lab
                     continue;
                 }
             }
+            for (int i = 0; i < c.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {c[i]}");
+            }
+            Console.WriteLine($"{c.Count + 1}. Add a car");
+            Console.WriteLine($"{c.Count + 2}. Quit");
+
             return c;
         }
-        public Car AddNew()
+        public static Car AddNew()
         {
             Console.WriteLine("What is the make of the car?");
             string make = Console.ReadLine();
@@ -73,7 +122,7 @@ namespace Car_Lot_Group_Lab
             Car result = new Car(make, model, year, price);
             return result;
         }
-        public Car AddUsed()
+        public static Car AddUsed()
         {
             Console.WriteLine("What is the make of the car?");
             string make = Console.ReadLine();
